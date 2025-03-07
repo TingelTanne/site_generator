88ea4c636f7bffd5e266b9b5bf8f208455efefba
TingelTanne <fuchs.tristan@yahoo.de>
2025-03-07 16:28:32 +0100

    text- to html-node converter added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -14,6 +14,34 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node3)
         self.assertNotEqual(node3, node4)
         self.assertNotEqual(node, urlNode)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        # Create a TextNode with type LINK - typically links have text and a URL
+        node = TextNode("Click me", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        
+        # Check that we got the right tag (a), value (the anchor text), 
+        # and href property (the URL)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click me")
+        self.assertEqual(html_node.props["href"], "https://example.com")
+
+    def test_image(self):
+        # Create a TextNode with type IMAGE - typically images have alt text and a URL
+        node = TextNode("Alt text for image", TextType.IMAGE, "https://example.com/image.jpg")
+        html_node = text_node_to_html_node(node)
+        
+        # Check that we got the right tag (img), empty value, and both src and alt properties
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")  # Image nodes have empty value
+        self.assertEqual(html_node.props["src"], "https://example.com/image.jpg")
+        self.assertEqual(html_node.props["alt"], "Alt text for image")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,20 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    TEXT = "text"
-    BOLD = "bold"
-    ITALIC = "italic"
+    TEXT = None
+    BOLD = "b"
+    ITALIC = "i"
     CODE = "code"
-    LINK = "link"
-    IMAGE = "image"
+    LINK = "a"
+    IMAGE = "img"
 
 class TextNode():
     
     def __init__(self, text, text_type:TextType, url = None):
+        if not isinstance(text_type, TextType):
+            raise ValueError(f"Invalid text_type: {text_type}. Must be a TextType enum value.")
+        
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -24,3 +28,12 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.LINK:
+        html_node = LeafNode("a", text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        html_node = LeafNode("img", value="", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        html_node = LeafNode(text_node.text_type.value, text_node.text)
+    return html_node
